c3e130713ac446a410279e79ab4b21d45e542fe8
Holiv <mrheltonso@gmail.com>
2022-06-03 16:35:33 -0300

    atualização de versão

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const btnToggle = document.getElementById('btn');
 function toggleMenu(){
     const nav = document.getElementById('navbar');
     nav.classList.toggle('active');
+    const psuedonav = document.getElementById('pseudo-nav');
+    psuedonav.classList.toggle('active');
 }
 
 btnToggle.addEventListener('click', toggleMenu);
